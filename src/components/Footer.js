65ec8713b4c1ce1d3c494814620a79649b6666ed
PritamSalunkhe1 <pritamkumar.s.ps@gmail.com>
2023-03-21 18:38:29 +0530

    Initial Commit.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='bg-bgDark py-4'>
+    <p className='text-1xl font-bold text-center text-white'>
+    Made with ❤️ by <a href="https://www.pritamsalunkhe.in" target={'_blank'}>PritamSalunkhe</a>
+    </p>
+    
+    </div>
+  )
+}
